e026bf805217dcb491b9f9fedc66836ed9e5c680
Charoline Septa <charoline.septa@si.ukdw.ac.id>
2018-03-14 18:57:39 +0700

    revert swagger

--- src/main/java/com/lolin/config/SwaggerConfiguration.java
+++ src/main/java/com/lolin/config/SwaggerConfiguration.java
@@ -1,22 +1,8 @@
-/*
 package com.lolin.config;
 
 
-*/
-/*
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-*//*
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -34,4 +20,3 @@ public class SwaggerConfiguration {
     }
 }
 
-*/
